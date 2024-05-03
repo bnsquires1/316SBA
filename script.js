0e751e9a87918fcf723d74b67ca031c456fcf407
Brittany Nicole <bnsquires1@gmail.com>
2024-05-03 13:26:23 -0400

    updates to html and beginning js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("TrackStar!")
+
+const todoInput = document.querySelector(".todo-input");
+
+const todoButton = document.getElementsByClassName("todo-button");
+
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo")
+
