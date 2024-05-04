e2d1fbfedc8d8b58c6899cde264397a8eeb473e0
Brittany Nicole <bnsquires1@gmail.com>
2024-05-04 02:20:54 -0400

    added second event listener and modifications

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ console.log("TrackStar!")
 
 window.addEventListener('load', () => {
     const form = document.querySelector("#form");
-    const input = document.querySelector(".todo-input");
+    const input = document.getElementById("todo-input");
     const listEl = document.querySelector("#tasks");
     
-})
+
 
 form.addEventListener('submit', (evt) => {
     evt.preventDefault();
@@ -13,7 +13,7 @@ form.addEventListener('submit', (evt) => {
     const task = input.value;
 
 if (!task) {
-    alert ("Please fill out the task");
+    alert("Please fill out the task");
     return;
 }
 
@@ -24,14 +24,49 @@ const taskCont = document.createElement("div");
 taskCont.classList.add("content");
 
 
+
 taskEl.appendChild(taskCont);
 
 const taskInput = document.createElement("input");
+
 taskInput.classList.add("text");
 taskInput.type = "text";
 taskInput.value = task;
+taskInput.setAttribute("readonly", "readonly");
 
-taskCont.appendChild(taskInput)
+
+taskCont.appendChild(taskInput);
+
+const actions = document.createElement("div");
+actions.classList.add("actions");
+
+const edit = document.createElement("button");
+edit.classList.add("edit");
+edit.innerHTML = "Edit"
+
+const deleteEl = document.createElement("button");
+deleteEl.classList.add("delete");
+deleteEl.innerHTML = "Delete";
+
+actions.appendChild(edit);
+actions.appendChild(deleteEl);
+
+taskEl.appendChild(actions);
 
 listEl.appendChild(taskEl);
+
+input.value = "";
+
+edit.addEventListener("click", (evt) => {
+    if (edit.innerText.toLowerCase() == "edit") {
+        taskInput.removeAttribute("readonly")
+        taskInput.focus();
+        edit.innerText = "Save";
+    } else {
+        console.log("Save");
+    }
+});
+
+})
+
 })
